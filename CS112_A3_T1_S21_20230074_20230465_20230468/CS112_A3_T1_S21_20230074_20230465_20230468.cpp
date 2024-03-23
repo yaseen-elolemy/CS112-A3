62ba993316e24f24e585a26f18751317b5e9bec1
Yaseen El-Olemy <yaseen.elolemy@gmail.com>
2024-03-23 21:15:20 +0200

    Update CS112_A3_T1_S21_20230074_20230465_20230468.cpp
    removed exiting function, cleaned up code and shrank the file to 38 lines from the previous 60 lines, increased readability

--- CS112_A3_T1_S21_20230074_20230465_20230468/CS112_A3_T1_S21_20230074_20230465_20230468.cpp
+++ CS112_A3_T1_S21_20230074_20230465_20230468/CS112_A3_T1_S21_20230074_20230465_20230468.cpp
@@ -2,51 +2,37 @@
 #include "Image_Class.h"
 using namespace std;
 
-void to_exit()
-{
-    cout<<"To exit the program pls enter (Exit)";
-    string to_exit;cin.ignore();
-    getline(cin,to_exit);
-    if (toupper(to_exit[0]) == 'E' && toupper(to_exit[1]) == 'X' && toupper(to_exit[2]) == 'I' && toupper(to_exit[3]) == 'T')
-    {
-        cout << "Thank you for using Editing Photo Program!" << endl;
-        exit(0);
-    }
-}
 
 int main()
 {
+    string image_name;
     cout<<"Welcome to Editing Photo Program"<<endl;
-    while(true)
+    while(image_name != "exit")
     {
-
-        cout<<"Pls enter the name of the image you want to edit: ";
-        string image_name;
+        image_name.clear();
+        cout<<"Pls enter the name of the image you want to edit or(exit): ";
         cin>>image_name;
-        Image image(image_name);
+        if(image_name!= "exit" )
+            {
+            Image image(image_name);
 
-        /*
-         cout<<"PLs choose the filter you want: ";
-         here we will insert the filters
+            /*
+             cout<<"PLs choose the filter you want: ";
+             here we will insert the filters
 
-         */
-        cout<<"Enter the new name of the photo you want ";
-        cout<<"and the format .jpg, .bmp, .png, .tga:";
-        cin>>image_name;
-        // when we activate the filters
-        // we can here make a choice to the user if he wants to save the new photo with the same name and same file or new file with new name
-
-        if(image.saveImage(image_name))
-        {
-            cout<<"changes saved successfully"<<endl;
+             */
+            cout<<"Enter the new name of the photo you want ";
+            cout<<"and the format .jpg, .bmp, .png, .tga:";
+            cin>>image_name;
+            // when we activate the filters
+                // we can here make a choice to the user if he wants to save the new photo with the same name and same file or new file with new name
+            if(image.saveImage(image_name))
+            {
+                cout<<"changes saved successfully"<<endl;
+            }
         }
-        // a function to exit program
-        to_exit();
-
+        
     }
-
-
-
-
+    return 0;
 
 }
