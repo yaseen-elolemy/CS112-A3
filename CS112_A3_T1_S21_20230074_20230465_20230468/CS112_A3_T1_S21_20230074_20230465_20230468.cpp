b4acf391b82c502e3f6679db835418c309c94530
Yassin Elsawy <161846098+yassinelsawy@users.noreply.github.com>
2024-03-24 08:17:43 +0200

    Update CS112_A3_T1_S21_20230074_20230465_20230468.cpp
    I added my first filter (Black and White)

--- CS112_A3_T1_S21_20230074_20230465_20230468/CS112_A3_T1_S21_20230074_20230465_20230468.cpp
+++ CS112_A3_T1_S21_20230074_20230465_20230468/CS112_A3_T1_S21_20230074_20230465_20230468.cpp
@@ -3,6 +3,40 @@
 using namespace std;
 
 
+void convertToBlackandWhite(Image& image)
+{
+    for(int i = 0; i<image.width;i++)
+    {
+        for(int j = 0; j<image.height;j++)
+        {
+            unsigned int avg = 0;
+            // accumulate the pixels value
+            for(int k = 0; k<image.channels;k++)
+            {
+                avg += image(i,j,k);
+            }
+            // calculate the average
+            avg/= 3;
+            for(int k = 0;k<3;k++)
+            {
+                // checking the brightness of each pixel to see if we will change the color to black or white
+                if(avg < 128)
+                {
+                    // changing all the colors to black
+                    image.setPixel(i,j,k,0);
+                }
+                else
+                {
+                    // changing all the colors to white
+                    image.setPixel(i,j,k,255);
+
+                }
+            }
+
+        }
+    }
+}
+
 int main()
 {
     string image_name;
